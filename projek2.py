977298644a78a037835a483ba76c56e522c0c99a
emodulTerbaru <emodulfives@gmail.com>
2025-01-04 15:01:18 +0700

    Update projek2.py

--- projek2.py
+++ projek2.py
@@ -5,6 +5,7 @@ import json
 
 cred_dict = st.secrets["firebase_key"]
 # Inisialisasi Firebase
+st.write(cred_dict)
 if not firebase_admin._apps:
     cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
